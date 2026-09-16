458a996805b3c68579098ee8c05c423acb264fe9
Fugakuuu <you@example.com>
2026-09-17 03:39:33 +0700

    pesan commit

--- components/about/polaroid-strip.tsx
+++ components/about/polaroid-strip.tsx
@@ -3,7 +3,6 @@
 import { motion, useMotionValue, useSpring, useTransform } from "motion/react";
 import { useRef, useSyncExternalStore, type ReactNode } from "react";
 
-import { DottedPattern } from "@/components/ui/dotted-pattern";
 
 type Polaroid = {
   id: string;
@@ -74,7 +73,6 @@ function PolaroidCard({
       }}
       className="relative aspect-[3/4] w-[clamp(6rem,11vw,9rem)] shrink-0 overflow-hidden rounded-2xl border-6 border-neutral-300/40 bg-white p-1.5 dark:border-white/15 dark:bg-neutral-900"
     >
-      <DottedPattern className="relative h-full w-full overflow-hidden rounded-xl" />
     </motion.div>
   );
 }
